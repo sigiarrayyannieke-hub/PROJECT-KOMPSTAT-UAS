6bd9be5c4149e3829e51247425cbf2f76bea244f
Sofie Indira Slafindy (1314624026) <sofies24a@gmail.com>
2026-07-11 21:44:25 +0700

    UI Visualisasi dan Statistika Deskriptif

--- STATEX_PROJECT_KOMPSTAT.R
+++ STATEX_PROJECT_KOMPSTAT.R
@@ -8,3 +8,24 @@ library(readxl)
 modus <- function(x){
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]}
+
+#UI
+ui <- navbarPage(title = "STATEX", 
+                 theme = bs_theme(bootswatch = "flatly", primary = "#0B3C8C"),
+  #Visualisasi
+  tabPanel("Visualisasi", br(), 
+           sidebarLayout(sidebarPanel(
+             fileInput("file", "Upload CSV atau Excel",
+                       accept = c(".csv", ".xlsx")),
+             uiOutput("pilih_var"), 
+             selectInput("grafik", "Pilih Grafik",
+                         c("Histogram", "Boxplot", "Bar Chart", "Pie Chart"))),
+             mainPanel(plotOutput("plot")))),
+  #Statistika Deskriptif
+  tabPanel("Statistika Deskriptif", br(),
+           sidebarLayout(sidebarPanel(
+             uiOutput("pilih_var2")),
+             mainPanel(h3("Hasil Statistika Deskriptif"),
+                       tableOutput("deskriptif"),
+                       br(), h3("Interpretasi"),
+                       verbatimTextOutput("interpretasi")))))
